346c14a508305dd462cc3c4d60e1f8b5c94ee5e7
yousends <101213452+yousends@users.noreply.github.com>
2024-02-02 11:42:51 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,16 +2,16 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp" # the link of manifest
+export BRANCH="twrp-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="surya" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_xiaomi_surya.git" # device tree link
+export DT_BRANCH="twrp_surya-eng-99.87.36-SP2A.220405.004-eng.rxuglr.20221127.174851-test-keys" # device tree branch
+export VENDOR="xiaomi" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="omni" # recovery type: omni, twrp, something else
+export TYPE="aosp" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
